15f08af492735c2116b65a42372e2be6b2b69358
felixleo22 <leofelixpro22@gmail.com>
2025-01-31 10:26:57 +0100

    fix: logger for production

--- services/api/src/lib/logger/access.ts
+++ services/api/src/lib/logger/access.ts
@@ -14,17 +14,26 @@ const apacheFormat = winston.format.printf((info: winston.Logform.TransformableI
   return `${info.timestamp} ${method} ${url} ${statusCode} ${contentLength} ${userAgent} ${responseTime}`;
 });
 
+const transports = nodeEnv === 'development'
+  ? [
+    new winston.transports.Console(),
+    new DailyRotateFile({
+      filename: `${paths.log.accessDir}/%DATE%-access.log`,
+      datePattern: 'YYYY-MM-DD',
+    }),
+  ]
+  : [
+    new DailyRotateFile({
+      filename: `${paths.log.accessDir}/%DATE%-access.log`,
+      datePattern: 'YYYY-MM-DD',
+    }),
+  ];
+
 export default winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
     apacheFormat,
   ),
-  transports: [
-    nodeEnv === 'development' ? new winston.transports.Console() : null,
-    new DailyRotateFile({
-      filename: `${paths.log.accessDir}/%DATE%-access.log`,
-      datePattern: 'YYYY-MM-DD',
-    }),
-  ],
+  transports,
 });
