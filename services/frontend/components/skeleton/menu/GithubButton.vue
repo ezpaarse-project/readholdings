b4e7643362fc268be8740da1454659edfaa16f7a
oxypomme <oxytom8@gmail.com>
2025-09-19 11:57:31 +0200

    refactor(frontend): refactored github button

--- services/frontend/components/skeleton/menu/GithubButton.vue
+++ services/frontend/components/skeleton/menu/GithubButton.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="text-center ma-2">
     <v-btn
-      small
       :href="gitHubRepo"
+      size="small"
+      variant="outlined"
+      append-icon="mdi-github"
       target="_blank"
       rel="noopener noreferrer"
-      variant="outlined"
     >
       Version: {{ version }}
-      <v-icon right>
-        mdi-github
-      </v-icon>
     </v-btn>
   </div>
 </template>
 
 <script setup>
-
 const runtimeConfig = useRuntimeConfig();
 
-const version = ref(runtimeConfig.public.version);
-const gitHubRepo = ref(runtimeConfig.public.gitHubRepoURL);
-
+const version = computed(() => runtimeConfig.public.version);
+const gitHubRepo = computed(() => runtimeConfig.public.gitHubRepoURL);
 </script>
