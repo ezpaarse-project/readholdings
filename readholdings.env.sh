19cc81e4920fd72fa7e5aaed9fdfa70d595cd580
felixleo22 <leofelixpro22@gmail.com>
2026-02-24 11:35:00 +0100

    refactor: cron

--- readholdings.env.sh
+++ readholdings.env.sh
@@ -4,9 +4,8 @@
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 LOCAL_ENV_FILE="$SCRIPT_DIR/readholdings.local.env.sh"
 
-export NODE_CONFIG='{ "smtp": { "secure": false, "ignoreTLS": true } }'
-
 # disabled it if you are in deploiement
+export NODE_CONFIG='{ "smtp": { "secure": false, "ignoreTLS": true } }'
 
 if [[ -f $LOCAL_ENV_FILE ]] ; then
   source "$LOCAL_ENV_FILE"
